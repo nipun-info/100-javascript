c37efbc964e5905c7f5f8c76683d3bb227e841d4
nipun shah <nipun.info.2020@gmail.com>
2024-01-27 11:27:22 +0600

    initilize 100 javascript folder

---
+++ task_one/index.js
@@ -0,0 +1,8 @@
+
+var payment = 1000;
+var orange = 200; var apples = 450;
+var purchesFruit = orange + apples;
+
+var result = payment - purchesFruit;
+console.log("The shopkeeper return cash " + result);
+
