2712e59ba5dd70cf00391b09ca6a01678c7e8d63
nipun shah <nipun.info.2020@gmail.com>
2024-02-11 10:54:00 +0600

    Module- 24 exercise

---
+++ DOM/dom.js
@@ -0,0 +1,15 @@
+
+
+// // const main = document.getElementById('main');
+// // console.log(main);
+
+// // document.getElementById("demo").innerHTML = "hello";
+
+// const sections = document.querySelectorAll('section');
+
+// // console.log(sections);
+
+// for (const section of sections) {
+//     console.log(section);
+// }
+
