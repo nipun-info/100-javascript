9324a9aa52e80f57c707db0ab6daa026dcea5794
nipun shah <nipun.info.2020@gmail.com>
2024-02-11 10:58:23 +0600

    create Module25 and update module24

--- DOM/Module24.js
+++ DOM/Module24.js
@@ -1,4 +1,6 @@
 
+
+
 // M-24.2: Traversing DOM - getElementsByTagName
 
 
@@ -11,7 +13,7 @@
 
 // 24.5: Dynamic Style, GetAttribute, SetAttribute, innerText, innerHTML
 
-24.6: Styling DOM Properties, add and Remove css class using JS
+// 24.6: Styling DOM Properties, add and Remove css class using JS
 // --------- use querySelector()
 const body = document.querySelector('body');
 body.style.background = 'azure';
@@ -36,7 +38,50 @@ fruitContainer.style.backgroundColor = "darkgray";
 fruitContainer.classList.add('text-center');
 fruitContainer.classList.add('text-color');
 
-24.7: NodeList, HTMLCollection, parentNode, childNodes, createElement
+// 24.7: NodeList, HTMLCollection, parentNode, childNodes, createElement
 
-24.8: Create HTML elements using Javascript and appendChild
+// 24.8: Create HTML elements using Javascript and appendChild
+
+
+
+// index.html code:
+const dom = `
+    <header>
+        <h1>JAVASCRIPT DOM MODULES</h1>
+    </header>
+    <main id="main-container">
+        <section id="courses">
+            <div>
+                <h1 id="title">My Awesome DOM</h1>
+            </div>
+            <div>
+                <ul class="courseList">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JAVASCRIPT</li>
+                    <li>REACT JS</li>
+                </ul>
+            </div>
+        </section>
+
+        <section id="fruits">
+            <div>
+                <h1>My Fav Fruits</h1>
+            </div>
+            <div>
+                <ul class="list">
+                    <li>Apple</li>
+                    <li>Banana</li>
+                    <li>Orange</li>
+                </ul>
+            </div>
+        </section>
+    </main>
+
+    <script src="script.js"></script>
+
+`
+
+
+
 
---
+++ DOM/Module25.js
@@ -0,0 +1,21 @@
+/*
+//*** Practice, Practice, Practice ------------
+M-25.1: What is Event, different types of event in web
+
+M-25.2: Add onclick handler directly or via javascript
+
+M-25.3: Different was to use addEventLister on a button
+
+M-25.4: Most common ways to set Event Handlers
+
+M-25.5: Create a comment box and display comment
+
+M-25.6: Simple github like delete confirmation button
+
+M-25.7: (semi-advanced) Event bubble and stop propagating
+
+M-25.8: (advanced) Event delegate and bebefit of Event bubble
+
+M-25.9: DOM events summary and homework
+M-25.10: Bsic js events all in one
+*/ 
